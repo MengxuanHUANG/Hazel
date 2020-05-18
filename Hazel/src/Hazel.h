f67ab39e18a23d150349b6f2c94b7e1a1e61326d
名字 <>
2020-05-18 20:22:43 +0800

    Set up basic Appliction and Entry point.

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// For used by Hazel applications
+
+#include<stdio.h>
+#include "Hazel/Application.h"
+
+// -------------Entry Points---------------
+#include "Hazel/EntryPoint.h"
+// -----------------------------------------
