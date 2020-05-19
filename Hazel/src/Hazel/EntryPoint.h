7eb5e3fac881c07048591fe542e287d3e57d9d46
名字 <>
2020-05-19 11:00:27 +0800

    Logging

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -6,6 +6,12 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv)
 {
+
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialized Log!");
+	int a=10;
+	HZ_CLIENT_INFO("Hello Var ={0:x}!", a);
+
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
