a9c9888b09bcee896f711a173108614d3b40c4cd
名字 <>
2020-06-04 18:56:46 +0800

    Add shader API for OpenGL

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -10,6 +10,7 @@
 
 #include "Hazel/ImGui/ImGuiLayer.h"
 
+#include "Hazel/Renderer/Shader.h"
 
 namespace Hazel {
 	class HAZEL_API Application
@@ -37,7 +38,9 @@ namespace Hazel {
 		LayerStack m_LayerStack;
 
 		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+		std::unique_ptr<Shader> m_Shader;
 
+	private:
 		static Application* s_Instance;
 	};
 
